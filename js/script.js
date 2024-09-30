c775f8ef62bde1142cf65201ca10873914c082a9
Andres14-Hernandez <andres14_hl@hotmail.com>
2024-09-30 16:54:14 -0500

    Detalles y Funcionamiento responsive

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
       if (this.hash !== "") {
         event.preventDefault();
   
-        var hash = this.hash;
-  
+        var hash = this.hash; 
         
         $('html, body').animate({
           scrollTop: $(hash).offset().top -100
